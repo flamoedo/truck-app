6dc88bb9d3313ec97c03dc684eec538181b6950b
flamoedo <fl.filho@yahoo.com.br>
2019-09-22 14:42:52 -0300

    Integração com Google API

--- src/views/List.vue
+++ src/views/List.vue
@@ -3,23 +3,20 @@
     <div id="AppTR" style="max-width: 400px; margin: auto;">
       <v-row>
         <v-col>
-          <v-card color="#385F73" dark>
+          <v-card color="#1F7087" dark>
             <v-card-text class="white--text">
               <div class="headline mb-2">{{search}}</div>
-              <!-- Listen to your favorite artists and albums whenever and wherever, online and offline. -->
             </v-card-text>
-
             <v-card-actions>
-              <!-- <v-btn text>Listen Now</v-btn> -->
+              <v-btn @click="proximo" text>Próximo</v-btn>
             </v-card-actions>
           </v-card>
         </v-col>
       </v-row>
-      <div v-if="posts.length > 0">
 
+      <div v-if="posts.length > 0">
         <v-row>
-          
-          <v-col v-for="post in posts" :key="post.title">
+          <v-col v-for="post in posts" :key="post.cacheId">
             <v-card>
               <v-list-item three-line>
                 <v-list-item-content class="align-self-start">
@@ -29,7 +26,7 @@
 
                 <v-list-item-avatar v-if="post.pagemap.cse_image" size="125" tile>
                   <div v-for="img in post.pagemap.cse_image" :key="img.src">
-                      <v-img height="120px" width="120px" :src="img.src"></v-img>
+                    <v-img height="120px" width="120px" :src="img.src"></v-img>
                   </div>
                 </v-list-item-avatar>
               </v-list-item>
@@ -44,7 +41,6 @@
   </v-content>
 </template>
 <script>
-
 export default {
   data: () => ({
     posts: [],
@@ -52,24 +48,35 @@ export default {
   }),
   mounted() {
     // for (var i = 0; i < 5; i++) {
-    fetch(
-      `https://www.googleapis.com/customsearch/v1?q=${this.search}&cx=007949313772641196886%3Aumrnh2lgtmc&key=AIzaSyDngYaB8_hSw79gLsOUaQyG0BhD7V9Zh1Y&start=1`
-    )
-      .then(response => response.json())
-      .then(data => {
-        if (!data.error) {
-          this.posts.push(...data.items);
-          // console.log(this.posts);          
-        } else {
-          this.error = data.error;
-          // console.log(this.posts);
-          // console.log(this.error);
-        }
-      });
+    this.pagina = 1;
+    this.getData(this.pagina);
   },
 
   created() {
     this.search = this.$route.params.search;
+  },
+  methods: {
+    proximo() {
+      this.pagina = this.pagina + 10;
+      this.getData(this.pagina);
+    },
+    getData(pagina) {
+      fetch(
+        `https://www.googleapis.com/customsearch/v1?q=${this.search}&cx=007949313772641196886%3Aumrnh2lgtmc&key=AIzaSyDngYaB8_hSw79gLsOUaQyG0BhD7V9Zh1Y&start=${pagina}`
+      )
+        .then(response => response.json())
+        .then(data => {
+          if (!data.error) {
+            this.posts = [];
+            this.posts.push(...data.items);
+            // console.log(this.posts);
+          } else {
+            this.error = data.error;
+            // console.log(this.posts);
+            // console.log(this.error);
+          }
+        });
+    }
   }
 };
 </script>
